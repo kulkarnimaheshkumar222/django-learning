7f389689fe2d701e6c17e9af696ccda33ea9704b
Mahesh Kulkarni <kulkarnimaheshkumar222@gmail.com>
2026-08-30 14:11:43 +0530

    implement CRUD operation

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, add_task, task_detail, edit_task, delete_task
 
 urlpatterns =[
     path("", home),
+    path("add/",add_task),
+    path("task/<int:id>/", task_detail),
+    path("task/<int:id>/edit/", edit_task),
+    path("task/<int:id>/delete/", delete_task),
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 def home(request):
@@ -9,3 +9,51 @@ def home(request):
     })
 
     return render(request,"todo_app/home.html")
+
+def add_task(request):
+
+    if request.method == "POST":
+        title = request.POST["title"]
+        description = request.POST["description"]
+
+        Task.objects.create(
+            title=title,
+            description=description
+        )
+
+        return redirect("/")
+
+    return render(request, "todo_app/add_task.html")
+
+def task_detail(request, id):
+    task = Task.objects.get(id=id)
+
+    return render(request, "todo_app/task_detail.html", {
+        "task": task
+    })
+
+def edit_task(request, id):
+
+    task = Task.objects.get(id=id)
+
+    if request.method == "POST":
+        task.title = request.POST["title"]
+        task.description = request.POST["description"]
+
+        task.save()
+
+        return redirect(f"/task/{task.id}/")
+
+    return render(request, "todo_app/edit_task.html", {
+        "task": task
+    })
+
+def delete_task(request, id):
+
+    task = Task.objects.get(id=id)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect("/")
+
+    return redirect(f"/task/{task.id}/")
